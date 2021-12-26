cb73211245c3e4e551eb8cf719b3c125f60de874
qyzhao <qyzhao@thoughtworks.com>
2021-12-26 19:48:00 +0800

    setup project with enzyme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,7 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import 'jest-enzyme'
+
+// 启动 enzyme 并添加对应 react 版本的适配器
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
